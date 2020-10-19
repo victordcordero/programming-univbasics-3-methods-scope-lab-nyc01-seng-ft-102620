916ff5eacb1da365801225797894f2fc4f30b98c
Victor Cordero <victordcordero@gmail.com>
2020-10-19 00:20:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,6 +6,7 @@ def mario
   puts phrase
 end
 
-def toadstool(status = 'Thank You Mario! But Our Princess Is In Another Castle!')
+def toadstool(status)
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
